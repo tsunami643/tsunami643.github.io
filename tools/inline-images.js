9dee4dd315dfaabcf5f053ac67c1fb9a0bb43b76
JT Burke <jtburke88@googlemail.com>
2015-08-18 14:02:26 +0100

    Before inlining preview portrait.

---
+++ tools/inline-images.js
@@ -0,0 +1,51 @@
+var fs = require('fs');
+var path = require('path');
+var Datauri = require('datauri');
+
+var tipsFolder = '../tips';
+
+fs.readdir(path.join(tipsFolder), function (err, files) {
+    if (err) {
+        return;
+    }
+
+    for (var i = 0; i < files.length; i++) {
+        var file = files[i];
+
+        (function(file) {
+            fs.readFile(path.join(tipsFolder, file), 'utf8', function (err, data) {
+                console.log(file);
+
+                if (err) {
+                    return console.log(err);
+                }
+
+                var regexp = /<img class="portrait-img" width="256" height="144" src="(.*?)"/g
+                var matches = regexp.exec(data);
+                var imagePath = path.join('../', matches[1]);
+
+                var datauri = new Datauri(imagePath).content;
+
+                var result = data.replace(
+                    /<img class="portrait-img" width="256" height="144" src="(.*?)" (.*?)>/g,
+                        '<img class="portrait-img" width="256" height="144" src="'+datauri+'" $2>'
+                );
+
+                if (!datauri) {
+                    console.log('- no data uri');
+                } else {
+                    console.log('- ok');
+                }
+
+                fs.writeFile(file, result, 'utf8', function (err) {
+                    if (err) {
+                        return console.log(err);
+                    }
+
+
+                    console.log('- written');
+                });
+            });
+        }(file));
+    }
+});
