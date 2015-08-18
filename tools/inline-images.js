585f12bd6809c752ed15c3d0fafd2c79e9e9bd90
JT Burke <jtburke88@googlemail.com>
2015-08-18 17:19:56 +0100

    Made inline images script more robust and have clearer output

--- tools/inline-images.js
+++ tools/inline-images.js
@@ -1,6 +1,13 @@
+/*
+    This is used to inline the hero profile images. If you'd like to change/update a
+    profile image just change the data-uri to the path of the file you'd like to be
+    in-lined and boom! It does it!
+ */
+
 var fs = require('fs');
 var path = require('path');
 var Datauri = require('datauri');
+var chalk = require('chalk');
 
 var tipsFolder = '../tips';
 
@@ -20,6 +27,12 @@ fs.readdir(path.join(tipsFolder), function (err, files) {
 
                 var regexp = /<img class="portrait-img" width="256" height="144" src="(.*?)"/g
                 var matches = regexp.exec(data);
+
+                if (!matches[1] || /data:image\/.*?base64/ig.test(matches[1])) {
+                    console.log(chalk.yellow(file  + ' - skipping'));
+                    return;
+                }
+
                 var imagePath = path.join('../', matches[1]);
 
                 var datauri = new Datauri(imagePath).content;
@@ -30,19 +43,12 @@ fs.readdir(path.join(tipsFolder), function (err, files) {
                 );
 
                 if (!datauri) {
-                    console.log(file  + ' - no data uri');
-                } else {
-                    console.log(file  + ' - ok');
+                    console.log(chalk.yellow(file  + ' - no data uri'));
                 }
 
-                fs.writeFile(path.join(tipsFolder, file), result, 'utf8', function (err) {
-                    if (err) {
-                        return console.log(err);
-                    }
+                fs.writeFileSync(path.join(tipsFolder, file), result, 'utf8');
 
-
-                    console.log(file  + ' - written');
-                });
+                console.log(chalk.green('✔ ' + file  + ' - inlined'));
             });
         }(file));
     }
