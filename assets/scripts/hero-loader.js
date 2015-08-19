910c9d1975741259f8e88fcd61f0472e778e498a
JT Burke <jtburke88@googlemail.com>
2015-08-19 17:40:00 +0100

    Testing javascript rewrite

---
+++ assets/scripts/hero-loader.js
@@ -0,0 +1,124 @@
+!function (global, $) {
+    function cachedImage(src) {
+        var imgEle = document.createElement('img');
+        imgEle.src = src;
+        return imgEle.complete || (imgEle.width + imgEle.height) > 0;
+    }
+
+    function preloadImage(src) {
+        var d = $.Deferred();
+        var cached = cachedImage(src);
+
+        function resolve() {
+            d.resolve({ src: src, cached: cached });
+        }
+
+        if (cached) {
+            resolve();
+        } else {
+            var img = new Image();
+            img.onload = function () { resolve(); };
+            img.src = src;
+        }
+
+        return d.promise(src);
+    }
+
+    /**
+     *
+     * @param options
+     * @constructor
+     */
+    function HeroLoader(options) {
+        this.loading = null;
+        this.cache = {};
+        this.currentHero = null;
+        this.urlFor = options.urlFor;
+        this.$el = options.$el;
+    }
+
+    HeroLoader.prototype = {
+        preload: function (hero) {
+            var _this = this;
+
+            if (!this.cache[hero]) {
+                this.cache[hero] = true;
+                $.get(this.urlFor(hero)).fail(function () {
+                    _this.cache[hero] = false;
+                });
+            }
+        },
+
+        collapse: function (skipAnimation) {
+            this.currentHero = null;
+            return $.Velocity.animate(this.$el, 'slideUp', { duration: skipAnimation ? 0 : 300});
+        },
+
+        expand: function (skipAnimation) {
+            return $.Velocity.animate(this.$el, 'slideDown', { duration: skipAnimation ? 0 : 500});
+        },
+
+        load: function (hero, skipAnimation) {
+            var _this = this;
+            var d = $.Deferred();
+
+            if (this.loading) {
+                this.loading.abort();
+            }
+
+            if (this.currentHero === hero) {
+                return d.promise();
+            }
+
+            var collapse = this.collapse(skipAnimation);
+
+            var request = $.get(this.urlFor(hero));
+            this.loading = request;
+
+            request.done(function (data) {
+                _this.cache[hero] = true;
+
+                collapse.then(function () {
+                    _this.$el.html(data);
+
+                    var $portrait = _this.$el.find('.portrait-img');
+
+                    var src = $portrait.data('src');
+
+                    if (src) {
+                        preloadImage(src).done(function (data) {
+                            var $frame = _this.$el.find('.portrait-frame');
+
+                            if (data.cached) {
+                                $frame.find('.portrait-img').addClass('from-cache');
+                            }
+
+                            $frame.prepend('<img class="portrait-img portrait-image-loaded" width="256" height="144" src="' + data.src + '">');
+                            $frame.addClass('portrait-frame-loaded');
+                        });
+                    }
+
+                    _this.expand(skipAnimation);
+                    _this.currentHero = hero;
+
+                    d.resolve();
+                });
+            });
+
+            request.fail(function () {
+                _this.cache[hero] = false;
+                _this.collapse();
+
+                d.reject();
+            });
+
+            request.always(function () {
+                _this.loading = null;
+            });
+
+            return d.promise();
+        }
+    };
+
+    global.HeroLoader = HeroLoader;
+}(this, this.jQuery);
---
+++ assets/scripts/main.js
@@ -0,0 +1,60 @@
+!function (global, document, $, HEROES) {
+    var heroes = new HeroList(HEROES.concat().sort());
+
+    var loader = new HeroLoader({
+        $el: $('#tipcontainer'),
+        urlFor: function (hero) {
+            return 'tips/' + hero.toLowerCase().replace(/ /ig, '_') + '.html';
+        }
+    });
+
+    var input = new HeroInput({
+        $el: $('#heroinput').find('.typeahead'),
+        $container: $('#inputline'),
+        heroes: heroes.all(),
+        anticipationDelay: 400
+    });
+
+    var state = new HeroState({
+        title: document.title
+    });
+
+    input.onAnticipate(function (e, hero) {
+        loader.preload(hero.name);
+    });
+
+    input.onSelect(function (e, hero) {
+        loader
+            .load(hero.name)
+            .then(function () {
+                state.setHero(hero.name);
+                input.collapse();
+                loader.preload(heroes.prev(hero.name));
+                loader.preload(heroes.next(hero.name));
+            })
+            .fail(function () {
+                input.setVal('');
+            });
+    });
+
+    input.onClear(function () {
+        state.setHero('');
+        loader.collapse();
+        input.expand();
+    });
+
+    var deepLinkedHero = state.getHero();
+
+    if (deepLinkedHero) {
+        loader.load(deepLinkedHero, true);
+        input.collapse(true);
+        input.setVal(deepLinkedHero);
+    }
+
+    $("#randomhero").click(function (e) {
+        e.preventDefault();
+        $(this).velocity({ rotateZ: '+=360' });
+        loader.load(deepLinkedHero);
+        input.setVal(heroes.random());
+    });
+}(this, this.document, this.jQuery, this.HEROES);
