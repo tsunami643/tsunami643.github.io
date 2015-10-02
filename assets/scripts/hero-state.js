3c818bbe14b9a5a72c24aebbd36f9e08fd3f457e
JT Burke <johnterrance.burke@autotrader.co.uk>
2015-10-02 22:53:39 +0100

    indentation

--- assets/scripts/hero-state.js
+++ assets/scripts/hero-state.js
@@ -1,25 +1,25 @@
 !function (global, History) {
-    function HeroState(options) {
-        this.title = options.title;
+  function HeroState(options) {
+    this.title = options.title;
+  }
+
+  HeroState.prototype = {
+    getHero: function () {
+      var state = History.getState();
+
+      return state.cleanUrl && state.cleanUrl.split('?')[1] && decodeURIComponent(state.cleanUrl.split('?')[1]);
+    },
+
+    setHero: function (hero) {
+      if (hero) {
+        document.title = hero + ' - ' + this.title;
+        History.replaceState(null, hero + ' - ' + this.title, '?' + encodeURIComponent(hero));
+      } else {
+        document.title = this.title;
+        History.replaceState(null, this.title, '?');
+      }
     }
+  };
 
-    HeroState.prototype = {
-        getHero: function () {
-            var state = History.getState();
-
-            return state.cleanUrl && state.cleanUrl.split('?')[1] && decodeURIComponent(state.cleanUrl.split('?')[1]);
-        },
-
-        setHero: function (hero) {
-            if (hero) {
-                document.title = hero + ' - ' + this.title;
-                History.replaceState(null, hero + ' - ' + this.title, '?' + encodeURIComponent(hero));
-            } else {
-                document.title = this.title;
-                History.replaceState(null, this.title, '?');
-            }
-        }
-    };
-
-    global.HeroState = HeroState;
+  global.HeroState = HeroState;
 }(this, this.History);
